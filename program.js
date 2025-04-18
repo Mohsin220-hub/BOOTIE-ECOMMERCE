4b98eef575c830ed60b1bb62c4873f8c267b8164
Mohsin220-hub <jammohsinali220@gmail.com>
2025-04-18 17:47:19 +0500

    first commit

---
+++ program.js
@@ -0,0 +1,124 @@
+// WHEN WE USE CLASS THEN USE JAVA
+// WHEN WE USE STRUCTURAL THEN USE JAVA SCRIPT
+// WHAT IS SCOPE
+
+
+// const myname='mohsin ali'
+// myname='hello'
+// console.log('this is myname',myname)
+
+// let myname='mohsin ali'
+// myname='hello'
+// console.log('this is myname',myname)
+
+// var myname='mohsin ali'
+// myname='hello'
+// console.log('this is myname',myname)
+
+
+
+// const myname='mohsin ali'
+// myname='hello'
+// console.log('this is myname',typeoof(myname))
+
+// const array=[1, 2,3, 4,5,]
+// console.log("the type of this =",array)
+
+// const a=25
+//  let b=30
+//   add=(a+b)
+
+//  console.log("sum is =",add)
+
+// const myname={
+//     name:"mohsin ali",
+//     contact:9832749283,
+//     email:"jason@gmail.com" 
+// }
+
+// console.log("this is function",myname)
+
+// const name="gsuy"
+// const cont=9789898
+// console.log(typeof("name"))
+
+// operators
+// arithematic[+, - ,*, / %, ++,--,]
+// logical[true o,false1 ]
+// assignment[=,==,===,+=,-=, etc]
+// comparison[>,<,??=nulish]
+// conditional
+
+
+// FUNCTIONS
+// reuseable block of code is called functions
+// what is acmascript
+// lexicalscoping
+// claisures
+// errors
+// Hosting
+// make a fuction and call it anywhere up and down is called hosting
+
+
+
+// console.log('hosting a variable')
+
+
+
+
+// function add(a,b){
+//    let multiply= a*b;
+//    let sub= a-b;
+//    // const Sum= priceOne + priceTwo
+
+//    console.log(`the multiple is ${multiply}`)
+// }
+// const uservalue = 20 
+// add(uservalue)
+   
+
+
+
+// const abc=add()
+// console.log()
+
+
+// const name = ()=>{
+
+// }
+// console.log(name())
+
+
+
+
+// function ageCalculator(age,a,b,c){
+//    const currentyear=2025;
+//    let userAge;
+//    let calculatedage= currentyear - age;
+
+//    console.log(`you are ${calculatedage} years old .`)
+//    return calculatedage;
+// }
+// let userAge=2005
+
+// ageCalculator(userAge)
+
+
+
+// LOOPS
+// WHILE LOOP
+// DO WHILE LOOP
+// FOR LOOP
+// FOR IN (OBJECT)
+// FOR OF (ARRAY)
+
+// const start=1
+// for(start=1; in start++){
+//    if(start===5){
+//       break
+//    }
+// }
+// console.log(`this is break` );
+
+
+
